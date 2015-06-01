2a8f43c3b15224a833432c7d9f4bece135747db4
baldeh110 <baldeh110@gmail.com>
2015-06-01 20:15:55 +0200

    Insertion librairie et classe test

--- Banque/src/Banque/Test.java
+++ Banque/src/Banque/Test.java
@@ -1,5 +1,42 @@
 package Banque;
 
+import java.io.File;
+
+import weka.classifiers.bayes.NaiveBayesUpdateable;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ArffLoader;
+	 /**
+	  * This example trains NaiveBayes incrementally on data obtained
+	  * from the ArffLoader.
+	  *
+	  * @author FracPete (fracpete at waikato dot ac dot nz)
+	  */
 public class Test {
+
+	   /**
+	    * Expects an ARFF file as first argument (class attribute is assumed
+	    * to be the last attribute).
+	    *
+	    * @param args        the commandline arguments
+	 * @throws Exception  if something goes wrong
+	    */
+	   public static void main(String[] args) throws Exception {
+	     // load data
+	     ArffLoader loader = new ArffLoader();
+	     loader.setFile(new File("C:/Users/thierno/Documents/M2/Info_Décisionnelle/Data Mining/Projet"));
+	     Instances structure = loader.getStructure();
+	     structure.setClassIndex(structure.numAttributes() - 1);
+
+	     // train NaiveBayes
+	     NaiveBayesUpdateable nb = new NaiveBayesUpdateable();
+	     nb.buildClassifier(structure);
+	     Instance current;
+	     while ((current = loader.getNextInstance(structure)) != null)
+	       nb.updateClassifier(current);
+
+	     // output generated model
+	     System.out.println(nb);
+	   }
 
 }
